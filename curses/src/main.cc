75fbb8896fdbb652b97c45ed3b6da4c91b353651
Oleg Keri <ezhi99@gmail.com>
2019-09-01 21:05:36 +0200

    Add space as connect hotkey

--- curses/src/main.cc
+++ curses/src/main.cc
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
                         return 0;
                     }
 
-                case 'C': case 'c':
+                case 'C': case 'c': case ' ':
                     connect();
                     break;
 
