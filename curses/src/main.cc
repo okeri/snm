0d747f2db510b62b3e947c8499d7f17889af6b23
Oleg Keri <ezhi99@gmail.com>
2020-02-01 12:10:15 +0100

    More proper way to handle errors replated to missing dbus adapter

--- curses/src/main.cc
+++ curses/src/main.cc
@@ -3,54 +3,58 @@
 #include "DBusLoop.hh"
 #include "Gui.hh"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "");
 
-    Gui gui;
+    auto gui = std::make_unique<Gui>();
     snm::NetworkManager networkManager(
         DBusLoop::connection(DBusLoop::Bus::System),
-        [&gui] (snm::ConnectionState&& state) {
-            gui.setNetworkState(std::move(state));
+        [&gui](snm::ConnectionState&& state) {
+            gui->setNetworkState(std::move(state));
         },
-        [&gui] (snm::ConnectionStatus status) {
-            gui.setNetworkStatus(status);
-        },
-        [&gui] (std::vector<snm::NetworkInfo>&& networks) {
-            gui.setNetworkList(std::move(networks));
+        [&gui](snm::ConnectionStatus status) { gui->setNetworkStatus(status); },
+        [&gui](std::vector<snm::NetworkInfo>&& networks) {
+            gui->setNetworkList(std::move(networks));
         });
+    try {
+        gui->setNetworkState(networkManager.get_state());
+    } catch (DBus::Error& error) {
+        gui.reset();
+        std::cerr << error.what() << std::endl;
+        return -1;
+    };
+    gui->setNetworkList(networkManager.get_networks());
+    gui->showNetworks();
 
-    gui.setNetworkState(networkManager.get_state());
-    gui.setNetworkList(networkManager.get_networks());
-    gui.showNetworks();
-
-    auto connect = [&networkManager, &gui] () {
-        gui.connect([&networkManager] (const snm::ConnectionId & id) {
-                networkManager.connect(id);
-            });
+    auto connect = [&networkManager, &gui]() {
+        gui->connect([&networkManager](const snm::ConnectionId& id) {
+            networkManager.connect(id);
+        });
     };
 
-    auto storeProps = [&networkManager, &gui] () {
-        auto [essid, props] = gui.getProps();
+    auto storeProps = [&networkManager, &gui]() {
+        auto [essid, props] = gui->getProps();
         networkManager.set_props(essid, props);
-        gui.showNetworks();
+        gui->showNetworks();
     };
 
     DBusLoop loop;
     while (true) {
         auto ch = getch();
-        if (auto handled = gui.pressed(ch); !handled) {
+        if (auto handled = gui->pressed(ch); !handled) {
             switch (ch) {
                 case KEY_LEFT:
-                    if (gui.display() == Gui::Display::Props) {
-                        gui.showNetworks();
+                    if (gui->display() == Gui::Display::Props) {
+                        gui->showNetworks();
                     }
                     break;
 
-                case 'P': case 'p':
+                case 'P':
+                case 'p':
                 case KEY_RIGHT:
-                    if (gui.display() == Gui::Display::Networks) {
-                        gui.showProps(
-                            [&networkManager] (const std::string &essid) {
+                    if (gui->display() == Gui::Display::Networks) {
+                        gui->showProps(
+                            [&networkManager](const std::string& essid) {
                                 return networkManager.get_props(essid);
                             });
                     } else {
@@ -59,29 +63,33 @@ int main(int argc, char *argv[]) {
                     break;
 
                 case KEY_ESC:
-                    if (gui.display() == Gui::Display::Props) {
-                        gui.showNetworks();
+                    if (gui->display() == Gui::Display::Props) {
+                        gui->showNetworks();
                     } else {
                         return 0;
                     }
 
-                case 'C': case 'c': case ' ':
+                case 'C':
+                case 'c':
+                case ' ':
                     connect();
                     break;
 
-                case 'D': case 'd':
+                case 'D':
+                case 'd':
                     networkManager.disconnect();
                     break;
 
                 case KEY_APPLY:
-                    if (gui.display() == Gui::Display::Networks) {
+                    if (gui->display() == Gui::Display::Networks) {
                         connect();
                     } else {
                         storeProps();
                     }
                     break;
 
-                case 'Q': case 'q':
+                case 'Q':
+                case 'q':
                     return 0;
             }
         }
