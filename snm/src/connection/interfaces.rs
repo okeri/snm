1418b65ec5252d933079bb73dce2a7938bd102d0
Oleg Keri <ezhi99@gmail.com>
2022-05-10 20:43:52 +0200

    Revert "Switch back to dhcpcd instead of smoltcp code :/"
    This reverts commit b5e43a3bdac41dec257de929cad4dd1944ca152b.

--- snm/src/connection/interfaces.rs
+++ snm/src/connection/interfaces.rs
@@ -2,7 +2,18 @@ use super::parsers::{parse, Parsers};
 use super::support;
 use super::types::{ConnectionInfo, ConnectionSetting};
 use nix::libc;
-use std::collections::HashSet;
+use smoltcp::dhcp::Dhcpv4Client;
+use smoltcp::iface::{EthernetInterfaceBuilder, NeighborCache, Routes};
+use smoltcp::phy::{wait, RawSocket};
+use smoltcp::socket::{RawPacketMetadata, RawSocketBuffer, SocketSet};
+use smoltcp::time::Instant;
+use smoltcp::wire::{EthernetAddress, IpCidr, Ipv4Address};
+use std::collections::{BTreeMap, HashSet};
+use std::os::unix::io::AsRawFd;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc, Mutex,
+};
 use std::{
     fmt, fs,
     hash::{Hash, Hasher},
@@ -10,11 +21,15 @@ use std::{
     thread, time,
 };
 
-const DHCP_TIMEOUT: u64 = 15;
+const DHCP_POLL_INTERVAL_MS: u64 = 500;
+const DHCP_TIMEOUT_MS: u64 = 10000;
+const DHCP_REST_CONFIGURED_MS: u64 = 10000;
 
 #[derive(Clone)]
 pub struct Interface {
     name: String,
+    dhcp_running: Arc<AtomicBool>,
+    ip: Arc<Mutex<String>>,
 }
 
 impl Hash for Interface {
@@ -35,16 +50,13 @@ impl Interface {
     pub fn new(name: &str) -> Self {
         Interface {
             name: name.to_owned(),
+            dhcp_running: Arc::new(AtomicBool::new(false)),
+            ip: Arc::new(Mutex::new(String::new())),
         }
     }
 
     pub fn disconnect(&self) {
-        let mut dhcpcd_exited = false;
-        while !dhcpcd_exited {
-            support::run("dhcpcd -x", false);
-            thread::sleep(time::Duration::from_millis(100));
-            dhcpcd_exited = support::run("pidof dhcpcd", false).is_empty();
-        }
+        self.dhcp_running.store(false, Ordering::SeqCst);
         support::run(&format!("ip addr flush dev {}", self.name), false);
         support::run(
             &format!("wpa_cli -i {} -p /var/run/wpa disconnect", self.name),
@@ -71,6 +83,7 @@ impl Interface {
     }
 
     pub fn down(&self) {
+        self.ip.lock().unwrap().clear();
         if self.valid() {
             support::run(&format!("ip l set {} down", self.name), false);
         }
@@ -100,23 +113,128 @@ impl Interface {
         !self.name.is_empty()
     }
 
+    fn dhcp_process(
+        ifname: &str,
+        mac: EthernetAddress,
+        runflag: Arc<AtomicBool>,
+        ip: Arc<Mutex<String>>,
+    ) {
+        let device = RawSocket::new(ifname).unwrap();
+        let fd = device.as_raw_fd();
+        let neighbor_cache = NeighborCache::new(BTreeMap::new());
+        let ip_addrs = [IpCidr::new(Ipv4Address::UNSPECIFIED.into(), 0)];
+        let mut routes_storage = [None; 1];
+        let routes = Routes::new(&mut routes_storage[..]);
+        let mut iface = EthernetInterfaceBuilder::new(device)
+            .ethernet_addr(mac)
+            .neighbor_cache(neighbor_cache)
+            .ip_addrs(ip_addrs)
+            .routes(routes)
+            .finalize();
+
+        let mut sockets = SocketSet::new(vec![]);
+        let dhcp_rx_buffer = RawSocketBuffer::new([RawPacketMetadata::EMPTY; 1], vec![0; 900]);
+        let dhcp_tx_buffer = RawSocketBuffer::new([RawPacketMetadata::EMPTY; 1], vec![0; 600]);
+
+        let mut dhcp =
+            Dhcpv4Client::new(&mut sockets, dhcp_rx_buffer, dhcp_tx_buffer, Instant::now());
+        let rest = time::Duration::from_millis(DHCP_REST_CONFIGURED_MS);
+        while runflag.load(Ordering::SeqCst) {
+            let timestamp = Instant::now();
+            if iface.poll(&mut sockets, timestamp).is_ok() {
+                if let Ok(config) = dhcp.poll(&mut iface, &mut sockets, timestamp) {
+                    if let Some(config) = config {
+                        config.address.map(|addr| {
+                            let new_ip = addr.address().to_string();
+                            if new_ip != "0.0.0.0" {
+                                let mut ip_locked = ip.lock().unwrap();
+                                if *ip_locked != new_ip {
+                                    *ip_locked = new_ip;
+                                    support::run(&format!("ip addr flush dev {}", ifname), false);
+                                    support::run(
+                                        &format!("ip addr add {} dev {}", addr, ifname),
+                                        false,
+                                    );
+                                }
+                            }
+                        });
+
+                        config.router.map(|route| {
+                            support::run(
+                                &format!("ip route add default via {} dev {}", route, ifname),
+                                false,
+                            );
+                        });
+
+                        if config.dns_servers.iter().any(|s| s.is_some()) {
+                            use std::io::Write;
+                            let mut resolv = std::fs::File::create("/etc/resolv.conf")
+                                .expect("cannot rewrite /etc/resolv.conf");
+                            for dns_server in config.dns_servers.iter().filter_map(|s| *s) {
+                                writeln!(resolv, "nameserver {}\n", dns_server)
+                                    .expect("cannot write /etc/resolve.conf");
+                            }
+                        }
+                    }
+                }
+            }
+            if !ip.lock().unwrap().is_empty() {
+                thread::sleep(rest);
+            }
+            let mut timeout = dhcp.next_poll(timestamp);
+            iface
+                .poll_delay(&sockets, timestamp)
+                .map(|sockets_timeout| timeout = sockets_timeout);
+            wait(fd, Some(timeout)).unwrap_or(());
+        }
+    }
+
     pub fn dhcp(&self) -> Result<String, ()> {
         if !self.valid() {
             return Err(());
         }
+        let mac = self.detect_mac().expect("cannot detect HW addr");
+        let ip = self.ip.clone();
+        let name = self.name.clone();
+        let runflag = self.dhcp_running.clone();
+        runflag.store(true, Ordering::SeqCst);
+        thread::spawn(move || {
+            Interface::dhcp_process(&name, mac, runflag, ip);
+        });
 
-        let output = support::run(
-            &format!("dhcpcd -t {} -i {} --waitip", DHCP_TIMEOUT, self.name),
-            true,
-        );
-        parse(Parsers::Ip, &output)
-            .map(|caps| caps[1].to_string())
-            .ok_or(())
+        let mut tries = 0;
+        let max_tries = DHCP_TIMEOUT_MS / DHCP_POLL_INTERVAL_MS;
+        while tries < max_tries {
+            thread::sleep(time::Duration::from_millis(DHCP_POLL_INTERVAL_MS));
+            let result = self.ip.lock().unwrap().clone();
+            if !result.is_empty() {
+                return Ok(result);
+            }
+            tries += 1;
+        }
+        self.dhcp_running.store(false, Ordering::SeqCst);
+        return Err(());
+    }
+
+    fn detect_mac(&self) -> Result<EthernetAddress, ()> {
+        use nix::{ifaddrs::getifaddrs, sys::socket::SockAddr};
+        let ifaces = getifaddrs().map_err(|_| ())?;
+
+        for iface in ifaces {
+            if iface.interface_name == self.name {
+                if let Some(addr) = iface.address {
+                    if let SockAddr::Link(link) = addr {
+                        return Ok(EthernetAddress(link.addr()));
+                    }
+                }
+            }
+        }
+        Err(())
     }
 
     fn detect_ip(&self) -> Option<String> {
         let ok: bool;
-        let ifreq = IfreqIp::new(&self.name);
+        let ifreq = ifreq_ip::new(&self.name);
         unsafe {
             let fd = libc::socket(libc::AF_INET, libc::SOCK_DGRAM, 0);
             ok = libc::ioctl(fd, libc::SIOCGIFADDR, &ifreq) != -1;
@@ -257,7 +375,7 @@ impl Interfaces {
 }
 
 #[repr(C)]
-struct SockaddrIn {
+struct sockaddr_in {
     pub sin_family: libc::sa_family_t,
     pub sin_port: libc::in_port_t,
     pub sin_addr: [u8; 4],
@@ -265,18 +383,18 @@ struct SockaddrIn {
 }
 
 #[repr(C)]
-struct IfreqIp {
+struct ifreq_ip {
     ifr_name: [libc::c_uchar; libc::IF_NAMESIZE],
-    pub ifr_addr: SockaddrIn,
+    pub ifr_addr: sockaddr_in,
 }
 
-impl IfreqIp {
+impl ifreq_ip {
     fn new(ifname: &str) -> Self {
         let mut ifr_name = [0; libc::IF_NAMESIZE];
         ifr_name[..ifname.len()].clone_from_slice(ifname.as_bytes());
-        Self {
+        ifreq_ip {
             ifr_name,
-            ifr_addr: SockaddrIn {
+            ifr_addr: sockaddr_in {
                 sin_family: libc::AF_INET as libc::sa_family_t,
                 sin_port: 0,
                 sin_addr: [0xff; 4],
